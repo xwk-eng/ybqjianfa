8a443d64219d2d127fdfe44470f692ac1b65cf7f
xwk-996 <3315893755>
2020-07-13 17:54:35 +0800

    xixingdafa

---
+++ src/main/java/com/riyueshenjiao/Xixingdafa.java
@@ -0,0 +1,14 @@
+package com.riyueshenjiao;
+
+public class Xixingdafa {
+    public static void main(String[] args) {
+        System.out.println("吸星大法好！！！！！！！！");
+        System.out.println("吸星大法好！！！！！！！！");
+        System.out.println("吸星大法好！！！！！！！！");
+
+
+
+    }
+
+
+}
